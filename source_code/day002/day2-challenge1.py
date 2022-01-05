a7dc3b9640c9ed41a709a3f177e5521fe87c017d
Michael Kutka <michael.kutka@gmail.com>
2022-01-04 20:48:54 -0500

    first commit

---
+++ source_code/day002/day2-challenge1.py
@@ -0,0 +1,16 @@
+#Day 2 Challenge 1!
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+a = two_digit_number[0]
+b = two_digit_number[1]
+
+print(int(a)+int(b))
+
+####################################
+#Single line version below
+print(int(two_digit_number[0])+int(two_digit_number[1]))
---
+++ source_code/day002/main.py
@@ -0,0 +1,36 @@
+#Data Types
+
+#String
+#Hello is quotes is the string or output
+print("Hello")
+
+#Subscript
+#looks at the position within the string and outputs just that character
+#below will output just H
+print("Hello"[0])
+
+#below will output just o
+print("Hello"[4])
+
+#Integer
+#actual numbers in the code for calculating
+#displayed just as numbers without quotes
+#below will add 123+456 = 579
+print(123+456)
+
+#in python 1000 is seperated with underscroll
+#this is to make it easier to read in the code
+print(1_000)
+print(125_000)
+
+#Float
+#This is used for decimals
+3.14159
+
+# #Boolean
+#data type which is always either True or False
+
+True
+False
+
+print(3 * (3 + 3) / 3 - 3)
